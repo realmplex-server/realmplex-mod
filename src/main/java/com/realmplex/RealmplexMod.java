9fd49ac35f50e0899a404fabc3050e2ad92b508f
clamlum2 <clamlum2@gmail.com>
2026-05-05 14:19:17 +1200

    wrote new config handler

--- src/main/java/com/realmplex/RealmplexMod.java
+++ src/main/java/com/realmplex/RealmplexMod.java
@@ -9,10 +9,11 @@ public class RealmplexMod implements ModInitializer {
     public static final String MOD_ID = "realmplex-mod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final RealmplexConfig.Config CONFIG = new RealmplexConfig().loadConfig();
+    public static BotConfig CONFIG;
 
     @Override
     public void onInitialize() {
+        CONFIG = BotConfig.load();
         LOGGER.info("Loaded Realmplex Mod");
         CurrencyConverter.register();
         ItemFlexer.register();
